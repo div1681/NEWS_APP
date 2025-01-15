58a7f85d2788ef7bdc9588829ead98fc1b939ad4
div1681 <g.divyansh@iitg.ac.in>
2025-01-16 04:54:11 +0530

    NEWS_DETAILS_added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/pages/news_details.dart';
 import 'package:news_app/pages/profile_page.dart';
 import 'package:news_app/utilities/get_news.dart';
 import 'package:news_app/utilities/my_button.dart';
@@ -113,96 +114,107 @@ class _HomePageState extends State<HomePage> {
                             final article = articles[index];
                             return Padding(
                               padding: const EdgeInsets.only(top: 25.0),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.amber[100],
-                                  borderRadius: BorderRadius.circular(18),
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Stack(
-                                      clipBehavior: Clip.none,
-                                      children: [
-                                        ClipRRect(
-                                          borderRadius: BorderRadius.only(
-                                            topLeft: Radius.circular(18),
-                                            topRight: Radius.circular(18),
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            NewsDetails(article: article),
+                                      ));
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.amber[100],
+                                    borderRadius: BorderRadius.circular(18),
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Stack(
+                                        clipBehavior: Clip.none,
+                                        children: [
+                                          ClipRRect(
+                                            borderRadius: BorderRadius.only(
+                                              topLeft: Radius.circular(18),
+                                              topRight: Radius.circular(18),
+                                            ),
+                                            child: Image.network(
+                                              article.urltoimage,
+                                              //height: 250,
+                                              width: double.infinity,
+                                              fit: BoxFit.contain,
+                                              loadingBuilder:
+                                                  (BuildContext context,
+                                                      Widget child,
+                                                      ImageChunkEvent?
+                                                          loadingProgress) {
+                                                if (loadingProgress == null)
+                                                  return child;
+                                                return Center(
+                                                  child:
+                                                      CircularProgressIndicator(
+                                                    value: loadingProgress
+                                                                .expectedTotalBytes !=
+                                                            null
+                                                        ? loadingProgress
+                                                                .cumulativeBytesLoaded /
+                                                            (loadingProgress
+                                                                    .expectedTotalBytes ??
+                                                                1)
+                                                        : null,
+                                                  ),
+                                                );
+                                              },
+                                            ),
                                           ),
-                                          child: Image.network(
-                                            article.urltoimage,
-                                            //height: 250,
-                                            width: double.infinity,
-                                            fit: BoxFit.cover,
-                                            loadingBuilder:
-                                                (BuildContext context,
-                                                    Widget child,
-                                                    ImageChunkEvent?
-                                                        loadingProgress) {
-                                              if (loadingProgress == null)
-                                                return child;
-                                              return Center(
-                                                child:
-                                                    CircularProgressIndicator(
-                                                  value: loadingProgress
-                                                              .expectedTotalBytes !=
-                                                          null
-                                                      ? loadingProgress
-                                                              .cumulativeBytesLoaded /
-                                                          (loadingProgress
-                                                                  .expectedTotalBytes ??
-                                                              1)
-                                                      : null,
+                                          Positioned(
+                                              top: 10,
+                                              right: 10,
+                                              child: IconButton(
+                                                onPressed: () {},
+                                                icon: Image.asset(
+                                                  'lib/icons/saved.png',
+                                                  height: 26,
+                                                  width: 26,
+                                                  color: Colors.amber[300],
                                                 ),
-                                              );
-                                            },
+                                              )),
+                                          Positioned(
+                                              top: 10,
+                                              left: 15,
+                                              child: Container(
+                                                  padding: EdgeInsets.all(8),
+                                                  child: Text(
+                                                    article.name,
+                                                    style: TextStyle(
+                                                        color: Colors.amber,
+                                                        fontSize: 18,
+                                                        fontWeight:
+                                                            FontWeight.bold),
+                                                  ))),
+                                        ],
+                                      ),
+                                      Container(
+                                        decoration: BoxDecoration(
+                                          color: Colors.amber[100],
+                                          borderRadius: BorderRadius.only(
+                                            bottomLeft: Radius.circular(18),
+                                            bottomRight: Radius.circular(18),
                                           ),
                                         ),
-                                        Positioned(
-                                            top: 10,
-                                            right: 10,
-                                            child: IconButton(
-                                              onPressed: () {},
-                                              icon: Image.asset(
-                                                'lib/icons/saved.png',
-                                                height: 32,
-                                                width: 32,
-                                                color: Colors.amber[300],
-                                              ),
-                                            )),
-                                        Positioned(
-                                            top: 10,
-                                            left: 15,
-                                            child: Container(
-                                                padding: EdgeInsets.all(8),
-                                                child: Text(
-                                                  article.name ?? '',
-                                                  style: TextStyle(
-                                                      color: Colors.amber,
-                                                      fontSize: 18,
-                                                      fontWeight:
-                                                          FontWeight.bold),
-                                                ))),
-                                      ],
-                                    ),
-                                    Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.amber[100],
-                                        borderRadius: BorderRadius.only(
-                                          bottomLeft: Radius.circular(18),
-                                          bottomRight: Radius.circular(18),
+                                        padding: EdgeInsets.all(12),
+                                        child: Text(
+                                          article.title,
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.black,
+                                          ),
                                         ),
                                       ),
-                                      padding: EdgeInsets.all(12),
-                                      child: Text(
-                                        article.title,
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
                               ),
                             );
--- lib/pages/news_details.dart
+++ lib/pages/news_details.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/utilities/article_class.dart';
 
 class NewsDetails extends StatelessWidget {
-  final List<String> news;
-  NewsDetails({required this.news});
+  final ArticleClass article;
+  NewsDetails({required this.article});
 
   @override
   Widget build(BuildContext context) {
@@ -18,43 +19,85 @@ class NewsDetails extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(18.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              news[0], // Title
-              style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.all(16),
-              height: 640,
-              width: 390,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: Colors.amber[100],
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 6,
-                    offset: Offset(2, 4),
-                  ),
-                ],
-              ),
-              child: Text(
-                news[1],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                article.description ?? '',
                 style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                  width: 390,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(14),
+                    child: Image.network(
+                      article.urltoimage,
+                      width: double.infinity,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(16),
+                width: 390,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.amber[100],
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 8,
+                      offset: Offset(2, 4),
+                    ),
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 8,
+                      offset: Offset(-2, 4),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      article.name,
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      article.content ?? '',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 60),
+                    Text(
+                      article.author ?? 'unknown',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       backgroundColor: Colors.amber[50],
